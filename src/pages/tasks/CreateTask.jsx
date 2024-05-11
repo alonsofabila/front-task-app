50eae2333824d315c2f89314674ca78f701531f9
alonsofabila <jorgealonsofabila@gmail.com>
2024-05-10 23:16:11 -0600

    (feature): Task creation form, delete task and get tasks for user.

---
+++ src/pages/tasks/CreateTask.jsx
@@ -0,0 +1,8 @@
+import CreateTaskForm from "../../components/task-components/CreateTaskForm.jsx";
+
+
+export function CreateTask() {
+    return (
+        <CreateTaskForm route='tasks/api/v1/task/' />
+    );
+}
